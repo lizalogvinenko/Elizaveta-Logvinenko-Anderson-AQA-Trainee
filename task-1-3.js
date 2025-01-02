f7b798bdd669eb4197ff3b5278edf7bc5595d27a
Elizaveta Logvinenko <elizaveta20logvinenko@gmail.com>
2025-01-02 11:16:29 +0400

    Corrected task-1-3 file and README file

--- task-1-3.js
+++ task-1-3.js
@@ -1,8 +1,7 @@
 /*
 This task does not specify a separator between the input numbers.
 For our solution, we can use a space " " as a separator.
-It is unclear whether 0 should be considered as part of the array of numbers that are multiples of 3.
-Let's assume that the condition with 0 is not applicable to our solution.
+It is unclear whether 0 is multiple of 3. Let's assume that is.
 Additionally, we can include some clarifying messages in case of incorrect input.
 */
 
@@ -14,13 +13,15 @@ const rl = readline.createInterface({
 });
 
 rl.question('Enter numbers (separated with space): ', (enteredText) => {
-    if (!enteredText.trim()) {
+    if (enteredText.trim().length === 0) {
         console.log('Input is empty');
         rl.close();
         return;
     }
 
-    const numberArray = enteredText.split(' ').map(Number);
+    const numberArray = enteredText.split(' ')
+        .filter((item) => item.length !== 0)
+        .map(Number);
 
     if (numberArray.some(isNaN)) {
         console.log('Please enter only numbers');
@@ -28,7 +29,7 @@ rl.question('Enter numbers (separated with space): ', (enteredText) => {
         return;
     }
 
-    const result = numberArray.filter(num => num % 3 === 0 && num !== 0);
+    const result = numberArray.filter(num => num % 3 === 0);
 
     console.log(result);
 
